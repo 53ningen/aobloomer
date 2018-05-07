7ae0da2a89a3e53d28d0383665c50d16c6a6ba67
gomi_ningen <gomi2ngen@gmail.com>
2018-05-07 22:48:59 +0900

    Copy title and url

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 
 function onClickHandler(info, tab) {
   switch (info.menuItemId) {
+    case "copy-title-url":
+      copyToClipboard(tab.title + "\n" + tab.url);
+      break;
     case "copy-page-with-md":
       copyToClipboard("[" + tab.title + "]" + "(" + tab.url + ")");
       break;
@@ -13,11 +16,12 @@ function onClickHandler(info, tab) {
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({"title": "Copy title and url", "id": "copy-title-url"});
+
   var contexts = ["page", "image"];
   for (var i = 0; i < contexts.length; i++) {
     const context = contexts[i];
     const title = "Copy " + context + "-link to clipboard with Markdown";
-    const id = chrome.contextMenus.create({"title": title, "contexts": [context], "id": "copy-" + context + "-with-md"});
-    console.log("'" + context + "' item:" + id);
+    chrome.contextMenus.create({"title": title, "contexts": [context], "id": "copy-" + context + "-with-md"});
   }
 });
